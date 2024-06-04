9a4ce04619385e783a829bb8116f3942a677ded0
Gonzalo Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2024-06-04 13:31:45 -0600

    Update Main.java

--- Queue/Main.java
+++ Queue/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
 
+        // Agregando elementos a la cola
+        queue.enqueue(10);
+        queue.enqueue(26);
+        queue.enqueue(36);
+
+        System.out.println("Elementos de la cola: ");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+    }
+}
