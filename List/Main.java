c7e2760443c089e977d0f7bec7f5b3c60a737b55
Gonzalo Portillo <160778946+GonzaPortillo@users.noreply.github.com>
2024-06-04 13:32:40 -0600

    Update Main.java

--- List/Main.java
+++ List/Main.java
@@ -1,1 +1,17 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Lista mylist = new Lista (5);
+        myList.add(10);
+        myList.add(20);
+        myList.add(30);
+        myList.add(40);
+        myList.add(50);
+        
+        System.out.println("Tamaño de lista: "+yList.size());
+        System.out.println("Elemento en el indice 2: "+ ylist.get(2));
+        System.out.println("¿La lista esta vacía? + List.isEmpty());
+        myList.remove(2);
+        System.out.println("Tamaño de la lista despues de eliminar el elemento del indice 2: ByList.size());
+        System.out.println("Elemento en el indice 2 despues de eliminar: mycist.get(2));
+    }
+}
